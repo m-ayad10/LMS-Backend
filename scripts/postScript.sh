4bf9639e11fdadb1f82b44df30756a1a38e841bd
Mohammed Ayad <ayadmohammed403@gmail.com>
2026-04-06 08:28:21 +0530

    Modified script file

--- scripts/postScript.sh
+++ scripts/postScript.sh
@@ -4,8 +4,10 @@ cd /home/ubuntu/node/LMS-Backend
 
 echo "Installing dependencies..."
 npm install
+npm audit fix
+npm audit fix --force
 
 echo "Restarting server..."
-pm2 start index.js
+pm2 restart backend
 
 echo "Deployment completed!"
